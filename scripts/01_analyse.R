e68c7cdf3ab962e68f4e3c1fe914ef6a5f8f2f1d
Ramdani Chaimae <rchaimae925@gmail.com>
2026-01-07 15:35:06 +0100

    R

--- scripts/01_analyse.R
+++ scripts/01_analyse.R
@@ -1,19 +1,50 @@
 
-library(tidyverse)  # Contient ggplot2, dplyr, readr
-library(gtsummary)  # Pour le tableau statistique 
-library(naniar)     # Pour les données manquantes 
-library(GGally)
+
+library(tidyverse)# manipulation des données + visualisation
+library(gtsummary)# tableaux statistiques propres
+library(naniar) # gestion des valeurs manquantes
+library(GGally)#graphiques exploratoires
+library(caret) # séparation train/test et évaluation
+library(pROC) # courbe ROC et AUC
+
 
 # --- PARTIE 1 : CHARGEMENT & NETTOYAGE ---
 data <- read.csv("data/churn_internet.csv", stringsAsFactors = TRUE)
 
+# Aperçu rapide des données
+head(data)      # premières lignes
+str(data)       # structure des variables
+summary(data)   # statistiques descriptives globales
+
+
+# Création d’un nouveau jeu de données "data_clean"
+# afin de conserver les données brutes intactes
+
 data_clean <- data %>%
   mutate(
-    charges.mensuelles = as.numeric(str_replace(charges.mensuelles, ",", ".")),
-    Charges.totales = as.numeric(str_replace(Charges.totales, ",", ".")),
-    Senior = factor(Senior, levels = c(0, 1), labels = c("Non", "Oui"))
+    # Les variables monétaires sont stockées en texte
+    # avec une virgule comme séparateur décimal.
+    # On remplace la virgule par un point puis on convertit en numérique.
+    charges.mensuelles = as.numeric(
+      str_replace(charges.mensuelles, ",", ".")
+    ),
+    
+    Charges.totales = as.numeric(
+      str_replace(Charges.totales, ",", ".")
+    ),
+    
+    # Transformation de la variable Senior (0 / 1)
+    # en variable catégorielle lisible
+    Senior = factor(
+      Senior,
+      levels = c(0, 1),
+      labels = c("Non", "Oui")
+    ),
+    
+    # Transformation explicite de la variable cible
+    # target = Oui (churn) / Non (pas de churn)
+    target = factor(target)
   )
-
 
 # Cela devrait afficher 11 NA dans "Charges.totales"
 print(colSums(is.na(data_clean)))
@@ -37,9 +68,80 @@ mon_tableau <- data_clean %>%
 
 print(mon_tableau) # S'affiche dans le Viewer
 
-# --- PARTIE 3 : Graphiques ---
 
-# Graphique : Senior vs Désabonnement
+#ANALYSE DESCRIPTIVE DE LA VARIABLE CIBLE
+# Nombre de clients churn / non churn
+table(data_clean$target)
+
+# Proportion de churn
+prop.table(table(data_clean$target))
+
+# Visualisation de la répartition du churn
+ggplot(data_clean, aes(x = target, fill = target)) +
+  geom_bar() +
+  labs(
+    title = "Répartition des clients selon le churn",
+    x = "Churn",
+    y = "Nombre de clients"
+  ) +
+  theme_minimal()
+
+#Visualisation 
+
+# Churn selon le type de contrat cela permet d’analyser l’impact de la durée d’engagement
+
+ggplot(data_clean, aes(x = Contrat, fill = target)) +
+  geom_bar(position = "fill") +
+  labs(
+    title = "Churn selon le type de contrat",
+    y = "Proportion de clients"
+  ) +
+  theme_minimal()
+
+
+# Charges mensuelles et churn : comparaison des distributions à l’aide d’un boxplot
+
+ggplot(data_clean, aes(x = target, y = charges.mensuelles, fill = target)) +
+  geom_boxplot() +
+  labs(
+    title = "Charges mensuelles selon le churn",
+    x = "Churn",
+    y = "Charges mensuelles"
+  ) +
+  theme_minimal()
+
+
+# Ancienneté des clients et churn -> Hypothèse que les clients récents quittent plus souvent
+
+ggplot(data_clean, aes(x = target, y = Anciennete, fill = target)) +
+  geom_boxplot() +
+  labs(
+    title = "Ancienneté des clients selon le churn",
+    y = "Ancienneté (en mois)"
+  ) +
+  theme_minimal()
+
+
+# Churn selon le statut Senior
+
+ggplot(data_clean, aes(x = Senior, fill = target)) +
+  geom_bar(position = "fill") +
+  labs(
+    title = "Churn selon le statut Senior",
+    y = "Proportion"
+  ) +
+  theme_minimal()
+
+
+# Tableau comparatif des variables numériques selon la variable cible avec test statistique
+
+data_clean %>%
+  select(target, charges.mensuelles, Charges.totales, Anciennete) %>%
+  tbl_summary(by = target) %>%
+  add_p()
+
+
+# Senior vs Désabonnement
 plot_senior <- data_clean %>%
   ggplot(aes(x = Senior, fill = target)) + 
   geom_bar(position = "fill") +            
@@ -55,7 +157,7 @@ plot_senior <- data_clean %>%
 print(plot_senior) 
 
 
-# 2. Deuxième Plot : La Matrice de Corrélation (Prix vs Ancienneté)
+# Plot : La Matrice de Corrélation (Prix vs Ancienneté)
 
 plot_corr <- data_clean %>%
   select(Anciennete, charges.mensuelles, Charges.totales, target) %>%
